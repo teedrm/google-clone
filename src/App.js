ce86980684d03716d950b2ec9fa2aa65e4bd9ffe
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-10 22:10:42 -0700

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM
     <div className="App">
-      <h1>Hello</h1>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/search" element={<h1>Search Page</h1>} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+
+//type rfce to get
+function Home() {
+  return (
+    <div className='home'>
+      <h1>Homepage</h1>
+
+      <div className='home_header'>
+        <div className='home_headerLeft'>
+
+        </div>
+        <div className='home_headerRight'>
+          
+          </div>
+      </div>
+
+      <div className='home_body'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
