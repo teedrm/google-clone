6e8c364ff13f84a64d631ecfeba180e195e0102f
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-10 23:26:38 -0700

    set up home header + link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import './Home.css'
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from '@material-ui/core';
+
 
 //type rfce to get
 function Home() {
   return (
     <div className='home'>
-      <h1>Homepage</h1>
-
       <div className='home_header'>
         <div className='home_headerLeft'>
-
+          {/* Use Link so there is no refresh*/}
+          <Link to="/about" >About</Link>
+          <Link to="/store" >Store</Link>
         </div>
         <div className='home_headerRight'>
-          
-          </div>
+          <Link to="/gmail" >Gmail</Link>
+          <Link to="/images" >Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
       </div>
 
       <div className='home_body'>
-        
+
       </div>
     </div>
   )
